a78db77b06e31fda110b51b6b45ca812af52ef85
sergiocouter <78119200+sergiocouter@users.noreply.github.com>
2022-07-26 15:56:00 -0300

    inclusão de imagens e correção de bugs

--- etapas.js
+++ etapas.js
@@ -1,42 +1,48 @@
 let etapas = [
   {
-    titulo: "VEREADOR",
+    titulo: 'VEREADOR',
     numeros: 5,
     candidatos: [
       {
-        numero: 38111,
-        nome: "Arthur Borges",
-        partido: "ABC",
-        fotos:[{url:"http://lorempixel.com.br/150/150", legenda: "Vereador", small: false}]
+        numero: '38111',
+        nome: 'Arthur Borges',
+        partido: 'ABC',
+        fotos: [
+          { url: 'imagens/arthur.png', legenda: 'Vereador', small: false }
+        ]
       },
       {
-        numero: 77222,
-        nome: "Pedro Henrique",
-        partido: "DEFG",
-        fotos:[{url:"http://lorempixel.com.br/150/150", legenda: "Vereador", small:false}]
-      },
+        numero: '77222',
+        nome: 'Pedro Henrique',
+        partido: 'DEFG',
+        fotos: [{ url: 'imagens/pedro.jpg', legenda: 'Vereador', small: false }]
+      }
     ]
   },
   {
-    titulo: "PREFEITO",
+    titulo: 'PREFEITO',
     numeros: 2,
     candidatos: [
       {
-        numero: 13,
-        nome: "Sérgio Couter",
-        partido: "ABC",
-        vice: "Silva Junior",
-        fotos: [{url: "http://lorempixel.com.br/150/150", legenda: "Prefeito"},
-                {url: "http://lorempixel.com.br/150/150", legenda: "Vice-Prefeito", small:true}]
+        numero: '10',
+        nome: 'Sérgio Couter',
+        partido: 'ABC',
+        vice: 'Silva Junior',
+        fotos: [
+          { url: 'imagens/prefeito.png', legenda: 'Prefeito' },
+          { url: 'imagens/vice.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
       },
       {
-        numero: 17,
-        nome: "Priscila Rochely",
-        partido: "DEFG",
-        vice: "Valdilena Moreira",
-        fotos: [{url: "http://lorempixel.com.br/150/150", legenda: "Prefeito"},
-                {url: "http://lorempixel.com.br/150/150", legenda: "Vice-Prefeito", small:true}]
-      },
+        numero: '15',
+        nome: 'Priscila Rochely',
+        partido: 'DEFG',
+        vice: 'Valdilena Moreira',
+        fotos: [
+          { url: 'imagens/prefeito.png', legenda: 'Prefeito' },
+          { url: 'imagens/vice.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
     ]
   }
-];
+]
--- script.js
+++ script.js
@@ -6,42 +6,124 @@ let lateral = document.querySelector(".d1_right");
 let numeros = document.querySelector(".d1_3");
 
 let etapaAtual = 0;
-let numero = '';
+let numero = ''; // numero que será digitado
+let votoBranco = false;
+let corrigir = true;
 
-function iniciarEtapa(){
-  let etapa = etapas[etapaAtual];
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = ''; // variavel pra colocar nos numeros
+    numero = '';
+    votoBranco = false;
+    corrigir = true;
 
-  let numeroHtml = '';
+    for (let i = 0; i < etapa.numeros; i++) {
+        if(i === 0){ // if para que a 1 posicao pisque
+            numeroHtml += '<div class="numero pisca"></div>';   
+        } else{
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
 
-  for (let index = 0; index < etapa.numeros; index++) {
-    numeroHtml += ' <div class="numero"></div>';
-    
-  }
-
-  seuVotoPara.style.display = 'none';
-  cargo.innerHTML = etapa.titulo;
-  descricao.innerHTML = '';
-  aviso.style.display = 'none';
-  lateral.innerHTML = '';
-  numeros.innerHTML = numeroHtml;
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
 }
 
 function atualizaInterface(){
-  
+    let etapa = etapas[etapaAtual];  
+    let candidato = etapa.candidatos.filter((item) => {
+        if(item.numero === numero) {
+            return true;
+        }else{
+            return false;
+        }
+    });
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    
+    if(candidato.length > 0){
+        candidato = candidato[0]; // caso tenha mais candidatos com o mesmo numero
+        descricao.innerHTML = `Nome: ${candidato.nome} <br>Partido: ${candidato.partido}<br>`;
+        if(candidato.vice !== undefined){ // se possuir vice
+            descricao.innerHTML += `Vice: ${candidato.vice}`;    
+        }
+        
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+          if(candidato.fotos[i].small){
+            fotosHtml += `<div class="d1_imagem small"><img src="${candidato.fotos[i].url}" alt="foto_prefeito">${candidato.fotos[i].legenda}</div>`;
+          } else{
+            fotosHtml += `<div class="d1_imagem"><img src="${candidato.fotos[i].url}" alt="foto_prefeito">${candidato.fotos[i].legenda}</div>`;
+          }
+          
+        }
+
+        lateral.innerHTML = fotosHtml;
+
+      }else{ // se não achar candidato 
+        descricao.innerHTML = '<div class="aviso_grande pisca"><br>VOTO NULO</div>';
+    }
 }
 
 function clicou(n){
-  alert(" Clicou em " + n)
+    let numeroP = document.querySelector('.numero.pisca');
+    if(numeroP !== null) {
+        numeroP.innerHTML = n;
+        numero += n;
+
+        numeroP.classList.remove('pisca');
+        if(numeroP.nextElementSibling !== null){
+            numeroP.nextElementSibling.classList.add('pisca');
+        } else{
+            atualizaInterface();
+        }
+    }    
 }
 
 function branco(){
-  alert(" Clicou em BRANCO! ")
+    if(numero === ''){
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso_grande_1 pisca">VOTO EM BRANCO</div>';            
+    } else{
+        alert('Para votar em BRANCO \no campo de voto deve estar vazio.\n'+ 
+            'Aperte CORRIGE para apagar o campo de voto.');
+    }
 }
 
 function corrige(){
-  alert(" Clicou em CORRIGE! ")
+    if(corrigir){ // variavel corrigir existe para não dar erro ao concluir tudo
+        comecarEtapa();
+    }
 }
 
 function confirma(){
-  alert(" Clicou em CONFIRMA! ")
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+    
+    if(votoBranco){
+        votoConfirmado = true;
+        console.log('Confirmando como voto BRANCO');
+    } else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        console.log('Confirmando como '+ numero);    
+    }
+    
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else{
+            corrigir = false;
+            document.querySelector('.tela').innerHTML = '<div class="aviso_gigante pisca">FIM</div>';    
+        }
+    }
 }
+
+comecarEtapa();
